de6e7cd32666b2513090b7bd779fa15c4b0d3a03
sigprecoodes <sig@precoodes.com>
2026-04-21 12:56:48 -0500

    Update config.js

--- config.js
+++ config.js
@@ -4,12 +4,12 @@ export const CONFIG = {
     'data/Cuadrilla_2.geojson',
     'data/Cuadrilla_3.geojson',
     'data/Cuadrilla_4.geojson',
-    'data/Cuadrilla_5.geojson',
+    'data/cuadrilla_5.geojson',
     'data/Cuadrilla_6.geojson',
     'data/Cuadrilla_7.geojson',
     'data/Cuadrilla_8.geojson',
     'data/Cuadrilla_9.geojson',
-    'data/Cuadrilla_11.geojson'
+    'data/cuadrilla_11.geojson'
   ],
   APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbzorYg7lSDUpYprv0wc4l_vdpHh7zO0uQGX96XZrmOoUwv6q_O-EEP_aVsrdeKY89TZ/exec',
   WEBAPP_URL: 'https://script.google.com/macros/s/AKfycbzorYg7lSDUpYprv0wc4l_vdpHh7zO0uQGX96XZrmOoUwv6q_O-EEP_aVsrdeKY89TZ/exec'
