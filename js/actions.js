f839519925409b469f60fe2238943f81f8dbcda0
sigprecoodes <sig@precoodes.com>
2026-04-25 11:29:02 -0500

    Update actions.js

--- js/actions.js
+++ js/actions.js
@@ -1,12 +1,26 @@
 import { buildAppUrl } from './api.js';
 
-export function abrirGestion({ microrruta, cuadrilla, lote }) {
-  const url = buildAppUrl({ page: 'gestion', microrruta, cuadrilla, lote });
+export function abrirGestion({ microrruta, cuadrilla, lote, quincena }) {
+  const url = buildAppUrl({
+    page: 'gestion',
+    microrruta,
+    cuadrilla,
+    lote,
+    quincena
+  });
+
   window.open(url, '_blank', 'noopener,noreferrer');
 }
 
-export function abrirReporteNovedad({ microrruta, cuadrilla, lote }) {
-  const url = buildAppUrl({ page: 'reporte', microrruta, cuadrilla, lote });
+export function abrirReporteNovedad({ microrruta, cuadrilla, lote, quincena }) {
+  const url = buildAppUrl({
+    page: 'reporte',
+    microrruta,
+    cuadrilla,
+    lote,
+    quincena
+  });
+
   window.open(url, '_blank', 'noopener,noreferrer');
 }
 
